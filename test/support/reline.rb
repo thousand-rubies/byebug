03dfd6c86583687b2062860757fff3c27f0e15e3
MSP-Greg <MSP-Greg@users.noreply.github.com>
2019-12-22 14:41:09 -0600

    reline.rb update

--- test/support/reline.rb
+++ test/support/reline.rb
@@ -3,7 +3,9 @@
 require "reline"
 
 unless STDOUT.tty? && STDIN.tty?
-  Reline::IOGate = Reline::GeneralIO
-
+  Reline.send(:remove_const, 'IOGate') if Reline.const_defined?('IOGate')
+  Reline.const_set('IOGate', Reline::GeneralIO)
+  Reline.send(:core).config.instance_variable_set(:@test_mode, true)
+  Reline.send(:core).config.reset
   Reline.input = $stdin
 end
