02e576724880598f0d380c43c9d5a0a83dde2172
MSP-Greg <MSP-Greg@users.noreply.github.com>
2019-12-22 16:42:24 -0600

    bundler

--- bin/setup.sh
+++ bin/setup.sh
@@ -5,7 +5,7 @@ set -eo pipefail
 set +x
 
 gem update --system 3.1.2 --conservative --no-document
-gem install bundler --version 2.1.4 --force
+gem install bundler --version 2.1.4 --force --conservative --no-document
 
 bundle install --jobs 3 --retry 3
 
